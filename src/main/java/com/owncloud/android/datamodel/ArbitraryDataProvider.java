d731c49a2d1406ab546101766f914a2e4a16caa2
AndyScherzinger <info@andy-scherzinger.de>
2018-04-22 23:02:10 +0200

    lint: close cursor

--- src/main/java/com/owncloud/android/datamodel/ArbitraryDataProvider.java
+++ src/main/java/com/owncloud/android/datamodel/ArbitraryDataProvider.java
@@ -209,6 +209,8 @@ public class ArbitraryDataProvider {
                     values.put(currentDir, new HashSet<>(valuesList));
                 }
             }
+            
+            cursor.close();
         } else {
             Log_OC.e(TAG, "DB error restoring arbitrary values.");
         }
