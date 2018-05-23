95b985f102e0fdc8e9ba6be14732dbbb4ef50878
ardevd <edvard.holst@gmail.com>
2018-05-23 10:37:45 +0200

    ui:preview Refactored full screen toggle gesture listener.
    Use PhotoView's own setOnPhotoTabListener() instead of the more generic setOnClickListener for capturing single taps on the photo.

--- src/main/java/com/owncloud/android/ui/preview/PreviewImageFragment.java
+++ src/main/java/com/owncloud/android/ui/preview/PreviewImageFragment.java
@@ -188,7 +188,7 @@ public class PreviewImageFragment extends FileFragment {
 
         view.setOnClickListener(v -> togglePreviewImageFullScreen());
 
-        mImageView.setOnClickListener(v -> togglePreviewImageFullScreen());
+        mImageView.setOnPhotoTapListener((view1, x, y) -> togglePreviewImageFullScreen());
 
         mMultiView = view.findViewById(R.id.multi_view);
 
@@ -445,6 +445,10 @@ public class PreviewImageFragment extends FileFragment {
 
     private void seeDetails() {
         mContainerActivity.showDetails(getFile());
+    }
+
+    private void downloadFile() {
+        ((PreviewImageActivity) getActivity()).requestForDownload(getFile());
     }
 
     @Override
@@ -739,9 +743,7 @@ public class PreviewImageFragment extends FileFragment {
         try {
             if (getActivity() != null) {
                 Snackbar.make(mMultiView, R.string.resized_image_not_possible_download, Snackbar.LENGTH_INDEFINITE)
-                        .setAction(R.string.common_yes, v ->
-                                ((PreviewImageActivity) getActivity())
-                                        .requestForDownload(getFile())).show();
+                        .setAction(R.string.common_yes, v -> downloadFile()).show();
             } else {
                 Snackbar.make(mMultiView, R.string.resized_image_not_possible, Snackbar.LENGTH_INDEFINITE).show();
             }
